9d09a146223e3f025cfc877bc64450be21777357
giamaf <gianluca.maffucci@hotmail.com>
2023-12-15 23:07:34 +0100

    added strong psw

--- js/script.js
+++ js/script.js
@@ -5,20 +5,18 @@ console.log('JS OK');
 const pswFixNumber = 23;
 console.log(pswFixNumber);
 
-
-
 // Richiesta informazioni (variabili)
-const userFirstName = prompt('Nome', 'Nome')
-const userLastName = prompt('Cognome', 'Cognome')
-const useFavColor = prompt('Colore Preferito', 'Colore Preferito')
-console.log(userFirstName, userLastName, useFavColor)
+const userFirstName = prompt('Nome', 'Nome');
+const userLastName = prompt('Cognome', 'Cognome');
+const useFavColor = prompt('Colore Preferito', 'Colore');
+console.log(userFirstName, userLastName, useFavColor);
 
 // Genera password quindi somma fattore fisso + variabile
 const psw = userFirstName + userLastName + useFavColor + pswFixNumber;
 console.log(psw);
 
 // Messaggio
-const message = 'La tua password è: '
+const message = 'La tua password è: ';
 
 // Stampa in HTML
-document.getElementById('result').innerHTML = message + psw;
+document.getElementById('result').innerHTML = message + '<strong>' + psw + '</strong>';
