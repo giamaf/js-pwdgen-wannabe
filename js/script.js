af01205b6868003b3f522e9c3b9e22d3dc01ccf8
giamaf <gianluca.maffucci@hotmail.com>
2023-12-15 18:47:06 +0100

    final result

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // Controllo collegamento JS
 console.log('JS OK');
+
+// Fattore fisso 
+const pswFixNumber = 23;
+console.log(pswFixNumber);
+
+
+
+// Richiesta informazioni (variabili)
+const userFirstName = prompt('Nome', 'Nome')
+const userLastName = prompt('Cognome', 'Cognome')
+const useFavColor = prompt('Colore Preferito', 'Colore Preferito')
+console.log(userFirstName, userLastName, useFavColor)
+
+// Genera password quindi somma fattore fisso + variabile
+const psw = userFirstName + userLastName + useFavColor + pswFixNumber;
+console.log(psw);
+
+// Messaggio
+const message = 'La tua password è: '
+
+// Stampa in HTML
+document.getElementById('result').innerHTML = message + psw;
